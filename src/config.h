d57b010aed137e28da712b824dbbe1104f7759b2
Daniel Gorzolnik <danielgorzolnik@gmail.com>
2022-12-23 13:09:00 +0100

    poprawne ustawo piny dla płytki 1.1

--- src/config.h
+++ src/config.h
@@ -1,24 +1,32 @@
 #define SOFT_VERSION 0xabcdef
 
 //pins
-#define pin_relay_1 13
-#define pin_relay_2 12
-#define pin_relay_3 11
-#define pin_relay_4 10
-#define pin_relay_5 9
-#define pin_relay_6 8
-#define pin_relay_7 7
-#define pin_relay_8 6
 
-#define pin_input_1 A5
-#define pin_input_2 A4
-#define pin_input_3 A3
-#define pin_input_4 A2
-#define pin_input_5 A1
-#define pin_input_6 A0
-#define pin_input_7 4
-#define pin_input_8 3
+//relays
+#define pin_relay_1 A1
+#define pin_relay_2 A0
+#define pin_relay_3 10
+#define pin_relay_4 9
+#define pin_relay_5 8
+#define pin_relay_6 7
+#define pin_relay_7 6
+#define pin_relay_8 5
 
-#define pin_uc_reset 5
+//input
+#define pin_input_1 4
+#define pin_input_2 3
+#define pin_input_3 A4
+#define pin_input_4 A3
+#define pin_input_5 A2
+#define pin_input_6 13
+#define pin_input_7 12
+#define pin_input_8 11
 
+//address select pin
+#define pin_address A7
+
+//uC self reset
+#define pin_uc_reset A5
+
+//max485 dir pin
 #define pin_dir_rs485 2
